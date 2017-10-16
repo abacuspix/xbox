56d1e35a82667ffa1a833e44aab64197d2a2a947
root <root@localhost.localdomain>
2017-10-16 23:10:29 +0800

    start rqscheduler in start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /usr/local/xbox
 python manage.py migrate
-python manage.py collectstatic
+nohup rqscheduler > /dev/null &
 python manage.py shell < scripts/init_db.py
 nohup python manage.py rqworker high default low > /dev/null &
